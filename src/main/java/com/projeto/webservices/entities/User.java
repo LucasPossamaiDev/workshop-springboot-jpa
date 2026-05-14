aee426e0063e60f4a03e1459bb7a33ec9a8a8dd5
LucasPossamaiDev <lucaspossamaidev@gmail.com>
2026-05-13 22:46:59 -0300

    H2 database, test profile, JPA

--- src/main/java/com/projeto/webservices/entities/User.java
+++ src/main/java/com/projeto/webservices/entities/User.java
@@ -1,11 +1,15 @@
 package com.projeto.webservices.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
